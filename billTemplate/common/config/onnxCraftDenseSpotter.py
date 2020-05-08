9cf81c38f4d149b4f1d80301e23d8cb0f1340747
root <root@localhost.localdomain>
2020-05-08 11:42:26 +0000

    adapt gpu

--- billTemplate/common/config/onnxCraftDenseSpotter.py
+++ billTemplate/common/config/onnxCraftDenseSpotter.py
@@ -1,31 +1,34 @@
 import os
 from yacs.config import CfgNode as CN
 
+
 detect_path = "/tmp/snapshot/detect/craft-b3hw.onnx"
-recog_path = "/tmp/snapshot/recog/model300.onnx"
+recog_path = "/tmp/snapshot/recog/model.onnx"
 
-width = 300
-path_cfg = dict(detect_path=detect_path, recog_path=recog_path)
-_C = CN(path_cfg, new_allowed=True)
+width = 600
+path_cfg = dict(detect_path=detect_path,
+                recog_path=recog_path)
+_C = CN(path_cfg,new_allowed=True)
 _C.model_cfg = CN()
-_C.model_cfg.recognizer = CN(
-    dict(height=32,
-         width=width,
-         batch_size=128,
-         input_name=['the_input:0'],
-         output_name=['y_pred/truediv:0'],
-         device='cpu'))
-_C.model_cfg.detector = CN(
-    dict(input_name=['actual_input_1'],
-         output_name=['output1'],
-         canvas_size=768,
-         mag_ratio=0.8,
-         text_threshold=0.4,
-         link_threshold=0.2,
-         low_text=0.4,
-         poly=True))
-_C.model_cfg.adapter = CN(dict(boxes_shape=(4, 2), output_size=(width, 32)))
+_C.model_cfg.recognizer = CN(dict(height=32,
+                               width=width,
+                               batch_size=128,
+                               input_name = ['the_input:0'],
+                               output_name=['y_pred/truediv:0'],
+                               device='cpu')
+                  )
+_C.model_cfg.detector = CN(dict(input_name = ['actual_input_1'],
+                                output_name=['output1'],
+                                canvas_size=768,
+                                mag_ratio = 0.8,
+                                text_threshold = 0.2,
+                                link_threshold = 0.2,
+                                low_text = 0.4,
+                                poly = True)
+                )
+_C.model_cfg.adapter = CN(dict(boxes_shape=(4,2),output_size=(width,32)))
 
-_C.return_coordinates = False
-_C.in_shape_type = 'rect'
-cfg = _C
+
+_C.return_coordinates=False
+_C.in_shape_type='rect'
+cfg=_C
--- test/test_client.py
+++ test/test_client.py
@@ -7,10 +7,10 @@ if __name__ == '__main__':
     # print(solution_dict)
     data = dict(solution=json.dumps(solution_dict))
     files = {'image_file': open('../sample/invoiceSample/5.jpg', 'rb')}
-    r = requests.post('http://0.0.0.0:9001/api/v1alpha1/inference',
+    r = requests.post('http://10.30.1.3:9010/api/v1alpha1/inference',
         files=files,data=data)
     content = json.loads(r.content,encoding='utf-8')
     print(content)
 
-    r = requests.get('http://0.0.0.0:9001/api/v1alpha1/status',)
+    r = requests.get('http://10.30.1.3:9010/api/v1alpha1/status',)
     print(r.content)
